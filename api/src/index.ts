78d7919a14c55412d98ce9d5678300de800912f8
Cristofer Gadens <gadens@Cristofers-MacBook-Air.local>
2025-02-22 02:04:41 -0300

    spot

---
+++ api/src/index.ts
@@ -0,0 +1,19 @@
+import path from 'node:path';
+import mongoose from "mongoose";
+import express from "express";
+
+
+import { router } from './router';
+
+mongoose.connect("mongodb://localhost:27017").then(() => {
+  const app = express();
+  const port = 3001;
+
+  app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
+  app.use(express.json());
+  app.use(router);
+
+  app.listen(port, () => {
+    console.log(`👁️ Server is running on http://localhost:${port}`);
+  });
+});
